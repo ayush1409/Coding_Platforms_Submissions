bab1dc49fac104134c720e0341aa5400086004ba
ayush <ayushsingh45556@gmail.com>
2022-01-10 16:57:26 +0530

    Check weather a subsequence is valid or not

---
+++ Algoexpert/validSubsequence.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+bool isValidSubsequence(vector<int> array, vector<int> sequence) {
+  int j = 0 ;
+	for(int i = 0 ; i < array.size() ; i++){
+		if(array[i] == sequence[j])
+			j++ ;
+	}
+	
+	if(j == sequence.size())
+		return true ;
+  return false;
+}
