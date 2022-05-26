02bc543cd3803f7398802d197c2a75938790ce60
ayush <ayushsingh45556@gmail.com>
2022-05-26 13:49:53 +0530

    compute pow(x,n)

---
+++ LeetCode/x_pow_n.cpp
@@ -0,0 +1,23 @@
+// Problem: https://leetcode.com/problems/powx-n/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0)
+            return 1 ;
+        if(n == 1)
+            return x ;
+        
+        double y = myPow(x, n/2) ;
+        
+        if(n % 2 == 0)
+            return y * y ;
+        else{
+            if(n > 0)
+                return x * y * y ;
+            else 
+                return (y*y)/x ;
+        }
+            
+    }
+};
